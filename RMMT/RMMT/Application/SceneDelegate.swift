26608468ed40314473930745ee337a4bf0a79593
Ekaterina V <katee.volkova@gmail.com>
2024-03-18 20:40:32 +0100

    feat: create empty project template

---
+++ RMMT/RMMT/Application/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  RMMT
+//
+//  Created by Kate Volkova on 18.03.24.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        setupWindow(for: scene)
+    }
+
+    private func setupWindow(for scene: UIScene) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = Task1ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ RMMT/RMMT/Tasks/Task1ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Task1ViewController.swift
+//  RMMT
+//
+//  Created by Kate Volkova on 18.03.24.
+//
+
+import UIKit
+
+final class Task1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        runTask()
+    }
+    
+    private func runTask() {}
+}
+
